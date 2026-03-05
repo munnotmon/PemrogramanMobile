2ac36de59a1a500faad308e3b04ce25d85b5dcc8
munnotmon <itoshimaul@gmail.com>
2026-03-05 12:05:18 +0700

    prak3 ganti repository

---
+++ DART/prak3/bin/prak3.dart
@@ -0,0 +1,11 @@
+import 'package:prak3/prak3.dart' as prak3;
+
+const String name = 'Muna';
+const int age = 21;
+void main(List<String> arguments) {
+  final String nama = 'Maulida';
+  final int umur = 20;
+
+  print('Nama saya $name, umur saya $age tahun');
+  print('Nama saya $nama, umur saya $umur tahun');
+}
